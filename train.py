1dadaf1738a262b4c7ec0bb3c3454dbc6ccd284b
Raghav Kavimandan <raghavkavimandan23@gmail.com>
2022-01-30 11:59:26 +0530

    eye detection feature added

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ while True:
     (cnts, _) = cv2.findContours(thresh_delta.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 40000:
+        if cv2.contourArea(contour) < 15000:
             continue
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
